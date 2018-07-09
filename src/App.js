fd09521214b6712dc0d3044870cdfa360f38945e
Mohammad Nhasa <Mohammad_N@mitrais.com>
2018-07-09 14:15:49 +0700

    Remove Boilerplate code and Show Dummy

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <table border="1" style={{ width: '100%' }}>
+          <tbody>
+            <tr>
+              <th rowspan="2">Tanggal</th>
+              <th rowspan="2">No</th>
+              <th rowspan="2">Keterangan</th>
+              <th colspan="2">Mutasi</th>
+              <th rowspan="2">+/-</th>
+              <th rowspan="2">Saldo</th>
+            </tr>
+            <tr>
+              <th>Debit</th>
+              <th>Kredit</th>
+            </tr>
+            <tr>
+              <td>1.9</td>
+              <td>0.003</td>
+              <td>40%</td>
+              <td>1.7</td>
+              <td>0.002</td>
+              <td>43%</td>
+              <td>TODO</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
